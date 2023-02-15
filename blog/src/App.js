42a3d8432b9593d1837272ef74a5aa1adc853cc3
shaswat97-crypto <shaswat97@gmail.com>
2023-02-15 21:28:04 +0530

    new proj

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,16 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
-import { store } from './app/store';
-import {Provider} from 'react-redux';
-import PostList from './features/post/PostList';
-import AddPostForm from './features/post/AddPostForm';
-import { fetchUsers } from './features/users/usersSlice';
+import logo from "./logo.svg";
+import "./App.css";
+import { store } from "./app/store";
+import { Provider } from "react-redux";
+import PostList from "./features/post/PostList";
+import AddPostForm from "./features/post/AddPostForm";
+import SinglePagePost from "./features/post/SinglePagePost";
+import { fetchUsers } from "./features/users/usersSlice";
+import { BrowserRouter as Routes, Route, Router } from "react-router-dom";
 store.dispatch(fetchUsers());
 function App() {
   return (
     <Provider store={store}>
-      <AddPostForm></AddPostForm>
-      <PostList></PostList>
+      <Routes>
+        <AddPostForm></AddPostForm>
+        <PostList></PostList>
+        <Route path='/' element= {<SinglePagePost></SinglePagePost>}></Route>
+      </Routes>
     </Provider>
   );
 }
--- blog/src/features/post/SinglePagePost.js
+++ blog/src/features/post/SinglePagePost.js
@@ -2,13 +2,12 @@ import React from 'react'
 import PostAuthor from "./PostAuthor";
 import TimeAgo from "./TimeAgo";
 import ReactionButtons from "./ReactionButtons";
-import { Outlet, Link } from "react-router-dom";
-function PostExcerpt({post}) {
+
+function SinglePagePost({post}) {
   return (
     <article>
       <h3>{post.title}</h3>
       <p>{post.body.substring(0, 100)}</p>
-      <Link to='/'>View Post</Link>
       <p className="postCredit">
         <PostAuthor userId={post.userId}></PostAuthor>
         <TimeAgo timestamp={post.date}></TimeAgo>
@@ -18,4 +17,4 @@ function PostExcerpt({post}) {
   )
 }
 
-export default PostExcerpt
+export default SinglePagePost
